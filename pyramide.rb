25d29fe9e6efcc3963c404b922531bd9f6211cc4
ftibi <thibault.folly@gmail.com>
2018-06-25 18:08:04 +0100

    user input has to be an integer

--- pyramide.rb
+++ pyramide.rb
@@ -1,9 +1,10 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 floors = 0
 begin
-  puts "Choisis un nombre entre 1 et 25"
-  floors = gets.to_i
-end while floors < 1 or floors > 25
+  puts "Choisis un nombre entier entre 1 et 25"
+  floors = gets.to_f
+end while floors != floors.to_i or floors < 1 or floors > 25
+floors = floors.to_i
 puts "Voici la pyramide :"
 for i in (1..floors)
   (floors - i).times do
